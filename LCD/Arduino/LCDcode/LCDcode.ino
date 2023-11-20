f830fffe4c42db9bb8279adf10bb2eb864ff2a75
Laura M <lamorenoca@unal.edu.co>
2023-11-20 14:24:07 +0100

    Main board file updated. Added Arduino code for the LCD
    The LCD is not working yet though.

--- LCD/Arduino/LCDcode/LCDcode.ino
+++ LCD/Arduino/LCDcode/LCDcode.ino
@@ -1,15 +1,34 @@
 #include <LiquidCrystal_I2C.h> // Library for LCD
-LiquidCrystal_I2C lcd(0x27, 20, 4); // I2C address 0x27, 16 column and 2 rows
+
+
+LiquidCrystal_I2C lcd(0x3D, 20, 4); // I2C address 0x3D (61), 20 column and 4 rows
 
 
 void setup() {
   // put your setup code here, to run once:
   lcd.init(); // initialize the lcd
   lcd.backlight();
+
+  // Reset
+  pinMode(8, OUTPUT);
+  digitalWrite(8, HIGH);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  
+
+  int i=0;
+  lcd.clear();                 // clear display
+  lcd.setCursor(0, 0);         // move cursor to   (0, 0)
+  lcd.print("Arduino");        // print message at (0, 0)
+  lcd.setCursor(2, 1);         // move cursor to   (2, 1)
+  lcd.print("This is a test"); // print message at (2, 1)
+  delay(2000);                 // display the above for two seconds
+
+  lcd.clear();                  // clear display
+  lcd.setCursor(3, 0);          // move cursor to   (3, 0)
+  lcd.print("Printing again");        // print message at (3, 0)
+  lcd.setCursor(0, 1);          // move cursor to   (0, 1)
+  lcd.print("Good bye"); // print message at (0, 1)
+  delay(2000); 
 
 }
